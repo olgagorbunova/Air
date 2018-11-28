704c11a492d862e9b47dad40536ceb21d75c4dd4
Olga Gorbunova <olgo1700@student.miun.se>
2018-11-28 11:21:27 +0000

    add eventListener to js and move script to <head>>

--- js/main.js
+++ js/main.js
@@ -4,29 +4,34 @@ olgo1700@student.miun.se
 */
 
 //Script detects user's webbrowser and updates a html tag with result
-let browserDetector = navigator.userAgent;
-let browserName;
 
-if (browserDetector.indexOf("Firefox") != -1) {
-     browserName = "Mozilla Firefox";
-}
-else if (browserDetector.indexOf("Opera") != -1) {
-     browserName = "Opera";
-}
-else if (browserDetector.indexOf("Trident") != -1) {
-     browserName = "Microsoft Internet Explorer";
-}
-else if (browserDetector.indexOf("Edge") != -1) {
-     browserName = "Microsoft Edge";
-}
-else if (browserDetector.indexOf("Chrome") != -1) {
-    browserName = "Google Chrome or Chromium";
-}
-else if (browserDetector.indexOf("Safari") != -1) {
-    browserName = "Apple Safari";
-}
-else {
-    browserName = "unable to detect";
+function detectBrowser() {
+  let browserDetector = navigator.userAgent;
+  let browserName;
+
+  if (browserDetector.indexOf("Firefox") != -1) {
+       browserName = "Mozilla Firefox";
+  }
+  else if (browserDetector.indexOf("Opera") != -1) {
+       browserName = "Opera";
+  }
+  else if (browserDetector.indexOf("Trident") != -1) {
+       browserName = "Microsoft Internet Explorer";
+  }
+  else if (browserDetector.indexOf("Edge") != -1) {
+       browserName = "Microsoft Edge";
+  }
+  else if (browserDetector.indexOf("Chrome") != -1) {
+      browserName = "Google Chrome or Chromium";
+  }
+  else if (browserDetector.indexOf("Safari") != -1) {
+      browserName = "Apple Safari";
+  }
+  else {
+      browserName = "unable to detect";
+  }
+
+  document.getElementById("browser").textContent = "Browser used: " + browserName;
 }
 
-document.getElementById("browser").textContent = "Browser used: " + browserName;
+window.addEventListener("load", detectBrowser);
