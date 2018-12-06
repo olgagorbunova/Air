3398f5f67c528a9cab44ac543a8e0d43fafa2c95
Olga Gorbunova <olgo1700@student.miun.se>
2018-12-06 17:00:32 +0000

    add js methods for displaying employee images and location detection

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ olgo1700
 olgo1700@student.miun.se
 */
 
-//Script detects user's webbrowser and updates a html tag with result
+/*COMMON METHODS*/
+//Detects current location and returns characters after last '/' as string
+function detectLocation() {
+  var pathArray = window.location.pathname.split('/');
+  return pathArray[pathArray.length-1];
+}
 
+/*CONTACT.HTML methods*/
+//Detects user's webbrowser and updates a html tag with result
 function detectBrowser() {
   let browserDetector = navigator.userAgent;
   let browserName;
@@ -30,8 +37,36 @@ function detectBrowser() {
   else {
       browserName = "unable to detect";
   }
-
   document.getElementById("browser").textContent = "Browser used: " + browserName;
 }
 
-window.addEventListener("load", detectBrowser);
+/*EMPLOYESS.HTML methods*/
+var imgNr; //image number argument for a showLargeImg
+
+function showLargeImg(imgNr) {
+  var largeImg = document.getElementById("pilotLarge");
+  largeImg.src = "img/pilot" + imgNr + ".jpg";
+}
+
+/*COMMON START METHOD*/
+//Registers event listeners / calls methods based on curremt location
+function start() {
+
+var myLocation = detectLocation();
+
+  if (myLocation == 'employees.html') {
+    var img1thumb  = document.getElementById("pilot1");
+    img1thumb.addEventListener("click", function() {showLargeImg(1);}, false);
+    var img2thumb  = document.getElementById("pilot2");
+    img2thumb.addEventListener("click", function() {showLargeImg(2);}, false);
+    var img3thumb  = document.getElementById("pilot3");
+    img3thumb.addEventListener("click", function() {showLargeImg(3);}, false);
+    var img4thumb  = document.getElementById("pilot4");
+    img4thumb.addEventListener("click", function() {showLargeImg(4);}, false);
+  }
+  else if (myLocation == 'contact.html') {
+    detectBrowser();
+  }
+}
+
+window.addEventListener("load", start, false);
